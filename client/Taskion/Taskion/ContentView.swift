cdda4c8dff28f6821e68e84a75851dddd404230f
beso1225 <yutaroformisc@gmail.com>
2026-01-07 10:59:22 +0900

    Add initial project structure and files for Taskion app
    - Created new Xcode project with SwiftUI framework.
    - Added main application file (TaskionApp.swift) and content view (ContentView.swift).
    - Included asset catalogs for app icon and accent color.
    - Set up unit and UI test targets with initial test files.
    - Added project configuration files and workspace settings.
    - Introduced database files for backend integration.

---
+++ client/Taskion/Taskion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taskion
+//
+//  Created by 髙木勇太朗 on 2026/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ client/Taskion/Taskion/TaskionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskionApp.swift
+//  Taskion
+//
+//  Created by 髙木勇太朗 on 2026/01/07.
+//
+
+import SwiftUI
+
+@main
+struct TaskionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
